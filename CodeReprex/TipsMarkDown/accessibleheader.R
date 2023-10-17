2ea7a2e733a191e67e447522b18fecd4338a9cf5
AK-MCH-EPI <jared.parrish@alaska.gov>
2023-10-17 14:49:00 -0800

    Add files via upload
    collapsible code in output, tabsets, internal links, and improving ADA accessibility of html R Markdown documents.

---
+++ CodeReprex/TipsMarkDown/accessibleheader.R
@@ -0,0 +1,6 @@
+## Improving accessibility  
+#' https://cran.r-project.org/web/packages/accessrmd/accessrmd.pdf 
+#' https://r-leyshon.github.io/access_rmd_docs/ 
+
+library(accessrmd)
+access_head(rmd_path = "TipsTricks.rmd", lan = "en")
